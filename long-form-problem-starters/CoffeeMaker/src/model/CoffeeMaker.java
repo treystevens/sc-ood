d1716a9f2fa2868e56041adbbf57a2d88c6670c9
treykris <trey10027@gmail.com>
2018-11-13 20:42:09 -0500

    Added implementation

--- long-form-problem-starters/CoffeeMaker/src/model/CoffeeMaker.java
+++ long-form-problem-starters/CoffeeMaker/src/model/CoffeeMaker.java
@@ -1,5 +1,7 @@
 package model;
 
+import model.exceptions.*;
+
 /**
  * A coffee maker used to train baristas.
  *
@@ -8,43 +10,77 @@ package model;
 
 public class CoffeeMaker {
 
+    private int remainingCups;
+    private int lastBrew;
+
     public CoffeeMaker(){
-        // TODO: complete the implementation of this method
+        remainingCups = 0;
+        lastBrew = 0;
     }
 
     // getters
     public int getTimeSinceLastBrew() {
-        // TODO: complete the implementation of this method
-        return 0;
+        return lastBrew;
     }
     public int getCupsRemaining() {
-        // TODO: complete the implementation of this method
-        return 0;
+        return remainingCups;
     }
 
     // EFFECTS: return true if there are coffee cups remaining
     public boolean areCupsRemaining() {
-        // TODO: complete the implementation of this method
-        return false;
+        return remainingCups > 0;
     }
 
     //REQUIRES: a non-negative integer
     //EFFECTS: sets time since last brew
     public void setTimeSinceLastBrew(int time) {
-        // TODO: complete the implementation of this method
+        lastBrew = time;
     }
 
-    //REQUIRES: beans between 2.40 and 2.60 cups, water > 14.75 cups
+
     //EFFECTS: sets cups remaining to full (20 cups) and time since last brew to 0
-    public void brew(double beans, double water){
-        // TODO: complete the implementation of this method
+    //         throws TooManyBeansException when beans > 2.60 cups
+    //         throws NotEnoughBeansException when < 2.40 cups
+    //         throws WaterException when water < 14.75cups
+    public void brew (double beans, double water) throws BeansAmountException, WaterException{
+        System.out.println("Starting to brew...");
+
+        if(beans > 2.60){
+            throw new TooManyBeansException(beans);
+        }
+        if(beans < 2.40){
+            throw new NotEnoughBeansException(beans);
+        }
+        if(water < 14.65){
+            throw new WaterException(water);
+        }
+
+        remainingCups = 15;
+        lastBrew = 0;
+
+        System.out.println("Finished brewing!");
     }
 
-    ///REQUIRES: cups remaining > 0, time since last brew < 60
+
     //MODIFIES: this
     //EFFECTS: subtracts one cup from cups remaining
-    public void pourCoffee() {
-        // TODO: complete the implementation of this method
+    //          throws NoCupsReaminingException when cups remaining == 0
+    //          throws StaleCoffeeException when the last brew > 60
+    public void pourCoffee() throws NoCupsRemainingException, StaleCoffeeException{
+        if(getCupsRemaining() == 0){
+            throw new NoCupsRemainingException();
+        }
+        else if(getTimeSinceLastBrew() > 60){
+            throw new StaleCoffeeException(getTimeSinceLastBrew());
+        }
+        else{
+            remainingCups--;
+
+            System.out.println("Pouring coffee... ");
+            System.out.println("Enjoy!");
+        }
+
+
     }
 
 

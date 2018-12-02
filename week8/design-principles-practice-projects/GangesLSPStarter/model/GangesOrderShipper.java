a904ef3def6fa1c0b2f25550362af10fa1cd97b6
treykris <trey10027@gmail.com>
2018-12-02 16:01:33 -0500

    Completed Todos - LSP

--- week8/design-principles-practice-projects/GangesLSPStarter/model/GangesOrderShipper.java
+++ week8/design-principles-practice-projects/GangesLSPStarter/model/GangesOrderShipper.java
@@ -12,7 +12,7 @@ public class GangesOrderShipper {
 
     public static void main(String[] args) {
         // Some books that need to be shipped
-        Book b1 = new Book("The Unbearable Lightness of Being John Malkovich", 19.95);
+        Book b1 = new StandardSizeBook("The Unbearable Lightness of Being John Malkovich", 19.95);
         Book b2 = new OversizedBook("The Princess Brideshead Revisited", 29.15);
 
         // Add them to a list so it's easy to iterate through them
@@ -24,11 +24,12 @@ public class GangesOrderShipper {
             String thisTitle = b.getTitle();
             System.out.printf("Packaging and shipping %s.\n", thisTitle);
 
-            // Package the book in a medium sized box
-            Box box = b.packageBook(new Box("medium"));
+            // Package the book in the minimum sized box required
+            String boxSize = b.getMinBoxSize();
+            Box box = b.packageBook(new Box(boxSize));
             double shipping = box.calculateShipping();
             System.out.printf("The book %s is being shipped for %.2f in a %s-sized box.\n",
-                    thisTitle, shipping, "medium");
+                    thisTitle, shipping, boxSize);
 
             // model.Book is successfully boxed up, so calculate the shipping cost and make sure it's not too expensive
             if (shipping > MAX_EXPECTED_SHIPPING) {
